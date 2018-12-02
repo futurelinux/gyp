f8a234cb44db5f278bf93a9ac365db4cf699c339
Dirk Pranke <dpranke@chromium.org>
2018-12-01 18:00:38 -0800

    Make GYP Python3-compatible.
    This patch is almost completely based off of
    https://codereview.chromium.org/1454433002/.
    
    NOT READY FOR LANDING!
    
    Change-Id: Iabee70148d26703653108403919a33a23fe6a400

--- test/mac/gyptest-strip-default.py
+++ test/mac/gyptest-strip-default.py
@@ -8,6 +8,8 @@
 Verifies that the default STRIP_STYLEs match between different generators.
 """
 
+from __future__ import print_function
+
 import TestGyp
 
 import re
@@ -17,6 +19,9 @@ import time
 
 if sys.platform == 'darwin':
   test = TestGyp.TestGyp(formats=['ninja', 'make', 'xcode'])
+
+  if test.format == 'xcode-ninja':
+    test.skip(bug=527)
 
   CHDIR='strip'
   test.run_gyp('test-defaults.gyp', chdir=CHDIR)
@@ -29,7 +34,7 @@ if sys.platform == 'darwin':
 
   def CheckNsyms(p, o_expected):
     proc = subprocess.Popen(['nm', '-aU', p], stdout=subprocess.PIPE)
-    o = proc.communicate()[0]
+    o = proc.communicate()[0].decode('utf8')
 
     # Filter out mysterious "00 0000   OPT radr://5614542" symbol which
     # is apparently only printed on the bots (older toolchain?).
@@ -40,8 +45,8 @@ if sys.platform == 'darwin':
     o = re.sub(r'^[a-fA-F0-9]+', 'XXXXXXXX', o, flags=re.MULTILINE)
     assert not proc.returncode
     if o != o_expected:
-      print 'Stripping: Expected symbols """\n%s""", got """\n%s"""' % (
-          o_expected, o)
+      print('Stripping: Expected symbols """\n%s""", got """\n%s"""' % (
+          o_expected, o))
       test.fail_test()
 
   CheckNsyms(OutPath('libsingle_dylib.dylib'),
